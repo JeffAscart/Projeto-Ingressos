48c31948ab0cbe2d21182a89d3ea070c5ca2b860
Jefferson Duraes <jefferson.vduraes@gmail.com>
2025-06-03 14:41:32 -0300

    Valores

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = document.getElementById('qtd');
+
+   // alert(tipo.value);
+   // alert(qtd.value);
+
+   
+
+
+}
